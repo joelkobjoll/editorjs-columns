bc71c985544fb074327c7157a51488c687fc35fd
Joel Kobjoll <kobjoll@gmail.com>
2022-06-26 21:15:00 +0200

    Add 4 columns design

--- src/editorjs-columns.js
+++ src/editorjs-columns.js
@@ -17,248 +17,263 @@ import Swal from "sweetalert2";
 import icon from "./editorjs-columns.svg";
 import style from "./editorjs-columns.css";
 
-import EditorJS from '@editorjs/editorjs'; // required for npm mode
+import EditorJS from "@editorjs/editorjs"; // required for npm mode
 
 class EditorJsColumns {
-	constructor({ data, config, api, readOnly }) {
-		this.api = api;
-		this.readOnly = readOnly;
-		this.config = config || {};
-		// console.log("HELLO")
-		// console.log(this.config)
-		// this.config.tools
-		// this.config.EditorJsLibrary !IMPORTANT
+  constructor({ data, config, api, readOnly }) {
+    this.api = api;
+    this.readOnly = readOnly;
+    this.config = config || {};
+    // console.log("HELLO")
+    // console.log(this.config)
+    // this.config.tools
+    // this.config.EditorJsLibrary !IMPORTANT
 
-		this._CSS = {
-			block: this.api.styles.block,
-			wrapper: "ce-EditorJsColumns",
-		};
+    this._CSS = {
+      block: this.api.styles.block,
+      wrapper: "ce-EditorJsColumns",
+    };
 
-		if (!this.readOnly) {
-			this.onKeyUp = this.onKeyUp.bind(this);
-		}
+    if (!this.readOnly) {
+      this.onKeyUp = this.onKeyUp.bind(this);
+    }
 
+    this._data = {};
 
-		this._data = {};
+    this.editors = {};
 
-		this.editors = {};
+    this.colWrapper = undefined;
 
-		this.colWrapper = undefined;
+    this.editors.cols = [];
 
-		this.editors.cols = [];
+    this.data = data;
 
-		this.data = data;
+    if (!Array.isArray(this.data.cols)) {
+      this.data.cols = [];
+      this.editors.numberOfColumns = 2;
+    } else {
+      this.editors.numberOfColumns = this.data.cols.length;
+    }
+  }
 
-		if (!Array.isArray(this.data.cols)) {
-			this.data.cols = [];
-			this.editors.numberOfColumns = 2;
-		} else {
-			this.editors.numberOfColumns = this.data.cols.length;
-		}
-	}
+  static get isReadOnlySupported() {
+    return true;
+  }
 
-	static get isReadOnlySupported() {
-		return true;
-	}
+  onKeyUp(e) {
+    if (e.code !== "Backspace" && e.code !== "Delete") {
+      return;
+    }
+  }
 
+  get CSS() {
+    return {
+      settingsButton: this.api.styles.settingsButton,
+      settingsButtonActive: this.api.styles.settingsButtonActive,
+    };
+  }
 
-	onKeyUp(e) {
-		if (e.code !== "Backspace" && e.code !== "Delete") {
-			return;
-		}
-	}
+  renderSettings() {
+    const buttonTwoCols = {
+      name: "Two Cols",
+      icon: `<div>2</div>`,
+    };
 
-	get CSS() {
-		return {
-			settingsButton: this.api.styles.settingsButton,
-			settingsButtonActive: this.api.styles.settingsButtonActive,
-		};
-	}
+    const buttonThreeCols = {
+      name: "Three Cols",
+      icon: `<div>3</div>`,
+    };
 
-	renderSettings() {
-		const buttonTwoCols = {
-			name: "Two Cols",
-			icon: `<div>2</div>`,
-		};
+    const buttonFourCols = {
+      name: "Four Cols",
+      icon: `<div>4</div>`,
+    };
 
-		const buttonThreeCols = {
-			name: "Three Cols",
-			icon: `<div>3</div>`,
-		};
+    const buttonRollCols = {
+      name: "Roll Cols",
+      icon: `<div>R</div>`,
+    };
 
-		const buttonRollCols = {
-			name: "Roll Cols",
-			icon: `<div>R</div>`,
-		};
+    const wrapper = document.createElement("div");
 
-		const wrapper = document.createElement("div");
+    let buttonTwoCols_Button = document.createElement("div");
+    buttonTwoCols_Button.classList.add("cdx-settings-button");
+    buttonTwoCols_Button.innerHTML = buttonTwoCols.icon;
 
-		let buttonTwoCols_Button = document.createElement("div");
-		buttonTwoCols_Button.classList.add("cdx-settings-button");
-		buttonTwoCols_Button.innerHTML = buttonTwoCols.icon;
+    let buttonThreeCols_Button = document.createElement("div");
+    buttonThreeCols_Button.classList.add("cdx-settings-button");
+    buttonThreeCols_Button.innerHTML = buttonThreeCols.icon;
 
-		let buttonThreeCols_Button = document.createElement("div");
-		buttonThreeCols_Button.classList.add("cdx-settings-button");
-		buttonThreeCols_Button.innerHTML = buttonThreeCols.icon;
+    let buttonFourCols_Button = document.createElement("div");
+    buttonThreeCols_Button.classList.add("cdx-settings-button");
+    buttonThreeCols_Button.innerHTML = buttonThreeCols.icon;
 
-		let buttonRollCols_Button = document.createElement("div");
-		buttonRollCols_Button.classList.add("cdx-settings-button");
-		buttonRollCols_Button.innerHTML = buttonRollCols.icon;
+    let buttonRollCols_Button = document.createElement("div");
+    buttonRollCols_Button.classList.add("cdx-settings-button");
+    buttonRollCols_Button.innerHTML = buttonRollCols.icon;
 
-		buttonTwoCols_Button.addEventListener("click", (event) => {
-			this._updateCols(2);
-		});
+    buttonTwoCols_Button.addEventListener("click", (event) => {
+      this._updateCols(2);
+    });
 
-		buttonThreeCols_Button.addEventListener("click", (event) => {
-			this._updateCols(3);
-		});
+    buttonThreeCols_Button.addEventListener("click", (event) => {
+      this._updateCols(3);
+    });
 
-		buttonRollCols_Button.addEventListener("click", (event) => {
-			this._rollCols();
-		});
+    buttonFourCols_Button.addEventListener("click", (event) => {
+      this._updateCols(4);
+    });
 
-		wrapper.appendChild(buttonTwoCols_Button);
-		wrapper.appendChild(buttonThreeCols_Button);
-		wrapper.appendChild(buttonRollCols_Button);
+    buttonRollCols_Button.addEventListener("click", (event) => {
+      this._rollCols();
+    });
 
-		return wrapper;
-	}
+    wrapper.appendChild(buttonTwoCols_Button);
+    wrapper.appendChild(buttonThreeCols_Button);
+    wrapper.appendChild(buttonFourCols_Button);
+    wrapper.appendChild(buttonRollCols_Button);
 
-	_rollCols() {
-		this.data.cols.unshift(this.data.cols.pop());
-		this.editors.cols.unshift(this.editors.cols.pop());
-		this._rerender();
-	}
+    return wrapper;
+  }
 
-	async _updateCols(num) {
-		// Should probably update to make number dynamic... but this will do for now
-		if (num == 2) {
-			if (this.editors.numberOfColumns == 3) {
-				let resp = await Swal.fire({
-					title: "Are you sure?",
-					text: "This will delete Column 3!",
-					icon: "warning",
-					showCancelButton: true,
-					confirmButtonColor: "#3085d6",
-					cancelButtonColor: "#d33",
-					confirmButtonText: "Yes, delete it!",
-				});
+  _rollCols() {
+    this.data.cols.unshift(this.data.cols.pop());
+    this.editors.cols.unshift(this.editors.cols.pop());
+    this._rerender();
+  }
 
-				if (resp.isConfirmed) {
-					this.editors.numberOfColumns = 2;
-					this.data.cols.pop();
-					this.editors.cols.pop();
-					this._rerender();
-				}
-			}
-		}
-		if (num == 3) {
-			this.editors.numberOfColumns = 3;
-			this._rerender();
-			// console.log(3);
-		}
-	}
+  async _updateCols(num) {
+    // Should probably update to make number dynamic... but this will do for now
+    if (num == 2) {
+      if (this.editors.numberOfColumns == 3) {
+        let resp = await Swal.fire({
+          title: "Are you sure?",
+          text: "This will delete Column 3!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!",
+        });
 
-	async _rerender() {
-		await this.save();
-		// console.log(this.colWrapper);
+        if (resp.isConfirmed) {
+          this.editors.numberOfColumns = 2;
+          this.data.cols.pop();
+          this.editors.cols.pop();
+          this._rerender();
+        }
+      }
+    }
+    if (num == 3) {
+      this.editors.numberOfColumns = 3;
+      this._rerender();
+    }
+    if (num == 4) {
+      this.editors.numberOfColumns = 4;
+      this._rerender();
+    }
+  }
 
-		for (let index = 0; index < this.editors.cols.length; index++) {
-			this.editors.cols[index].destroy();
-		}
-		this.editors.cols = [];
+  async _rerender() {
+    await this.save();
+    // console.log(this.colWrapper);
 
-		this.colWrapper.innerHTML = "";
+    for (let index = 0; index < this.editors.cols.length; index++) {
+      this.editors.cols[index].destroy();
+    }
+    this.editors.cols = [];
 
-		// console.log("Building the columns");
+    this.colWrapper.innerHTML = "";
 
-		for (let index = 0; index < this.editors.numberOfColumns; index++) {
-			// console.log("Start column, ", index);
-			let col = document.createElement("div");
-			col.classList.add("ce-editorjsColumns_col");
-			col.classList.add("editorjs_col_" + index);
+    // console.log("Building the columns");
 
-			let editor_col_id = uuidv4();
-			// console.log("generating: ", editor_col_id);
-			col.id = editor_col_id;
+    for (let index = 0; index < this.editors.numberOfColumns; index++) {
+      // console.log("Start column, ", index);
+      let col = document.createElement("div");
+      col.classList.add("ce-editorjsColumns_col");
+      col.classList.add("editorjs_col_" + index);
 
-			this.colWrapper.appendChild(col);
+      let editor_col_id = uuidv4();
+      // console.log("generating: ", editor_col_id);
+      col.id = editor_col_id;
 
-			let editorjs_instance = new EditorJS({
-				defaultBlock: "paragraph",
-				holder: editor_col_id,
-				tools: this.config.tools,
-				data: this.data.cols[index],
-				readOnly: this.readOnly,
-				minHeight: 50,
-			});
+      this.colWrapper.appendChild(col);
 
-			this.editors.cols.push(editorjs_instance);
-		}
-	}
+      let editorjs_instance = new EditorJS({
+        defaultBlock: "paragraph",
+        holder: editor_col_id,
+        tools: this.config.tools,
+        data: this.data.cols[index],
+        readOnly: this.readOnly,
+        minHeight: 50,
+      });
 
-	render() {
-		// console.log("Generating Wrapper");
+      this.editors.cols.push(editorjs_instance);
+    }
+  }
 
-		// console.log(this.api.blocks.getCurrentBlockIndex());
+  render() {
+    // console.log("Generating Wrapper");
 
-		this.colWrapper = document.createElement("div");
-		this.colWrapper.classList.add("ce-editorjsColumns_wrapper");
+    // console.log(this.api.blocks.getCurrentBlockIndex());
 
-		for (let index = 0; index < this.editors.cols.length; index++) {
-			this.editors.cols[index].destroy();
-		}
+    this.colWrapper = document.createElement("div");
+    this.colWrapper.classList.add("ce-editorjsColumns_wrapper");
 
-		// console.log(this.editors.cols);
-		this.editors.cols = []; //empty the array of editors
-		// console.log(this.editors.cols);
+    for (let index = 0; index < this.editors.cols.length; index++) {
+      this.editors.cols[index].destroy();
+    }
 
-		// console.log("Building the columns");
+    // console.log(this.editors.cols);
+    this.editors.cols = []; //empty the array of editors
+    // console.log(this.editors.cols);
 
-		for (let index = 0; index < this.editors.numberOfColumns; index++) {
-			// console.log("Start column, ", index);
-			let col = document.createElement("div");
-			col.classList.add("ce-editorjsColumns_col");
-			col.classList.add("editorjs_col_" + index);
+    // console.log("Building the columns");
 
-			let editor_col_id = uuidv4();
-			// console.log("generating: ", editor_col_id);
-			col.id = editor_col_id;
+    for (let index = 0; index < this.editors.numberOfColumns; index++) {
+      // console.log("Start column, ", index);
+      let col = document.createElement("div");
+      col.classList.add("ce-editorjsColumns_col");
+      col.classList.add("editorjs_col_" + index);
 
-			this.colWrapper.appendChild(col);
+      let editor_col_id = uuidv4();
+      // console.log("generating: ", editor_col_id);
+      col.id = editor_col_id;
 
-			let editorjs_instance = new EditorJS({
-				defaultBlock: "paragraph",
-				holder: editor_col_id,
-				tools: this.config.tools,
-				data: this.data.cols[index],
-				readOnly: this.readOnly,
-				minHeight: 50,
-			});
+      this.colWrapper.appendChild(col);
 
-			this.editors.cols.push(editorjs_instance);
-			// console.log("End column, ", index);
-		}
-		return this.colWrapper;
-	}
+      let editorjs_instance = new EditorJS({
+        defaultBlock: "paragraph",
+        holder: editor_col_id,
+        tools: this.config.tools,
+        data: this.data.cols[index],
+        readOnly: this.readOnly,
+        minHeight: 50,
+      });
 
-	async save() {
-		if(!this.readOnly){
-			// console.log("Saving");
-			for (let index = 0; index < this.editors.cols.length; index++) {
-				let colData = await this.editors.cols[index].save();
-				this.data.cols[index] = colData;
-			}
-		}
-		return this.data;
-	}
+      this.editors.cols.push(editorjs_instance);
+      // console.log("End column, ", index);
+    }
+    return this.colWrapper;
+  }
 
-	static get toolbox() {
-		return {
-			icon: icon,
-			title: "Columns",
-		};
-	}
+  async save() {
+    if (!this.readOnly) {
+      // console.log("Saving");
+      for (let index = 0; index < this.editors.cols.length; index++) {
+        let colData = await this.editors.cols[index].save();
+        this.data.cols[index] = colData;
+      }
+    }
+    return this.data;
+  }
+
+  static get toolbox() {
+    return {
+      icon: icon,
+      title: "Columns",
+    };
+  }
 }
 
 export { EditorJsColumns as default };
